ef5467e65389f8766a592c800b912c195c2b33d6
NataliaMahera <116887958+NataliaMahera@users.noreply.github.com>
2023-08-06 23:22:13 +0200

    add hw

--- task-01.js
+++ task-01.js
@@ -1,21 +1,52 @@
-const totalCategories = document.querySelectorAll(".item");
-console.dir(`Number of categories: ${totalCategories.length}`);
-
-// totalCategories.forEach(category => {
-//     const heading = document.querySelector("h2").innerHTML
-//     console.log(`Category: ${heading.textContent}
-//     Elements: ${heading.children.length}`)})
-
-const items = document.querySelectorAll('.item');
-Array.prototype.forEach.call(items, (element) => {
-    const title = element.querySelector('h2').innerHTML;
-    const itemsLength = element.querySelectorAll('li').length;
-    console.log(`Категория: ${title} / Количество элементов: ${itemsLength}`);
-});
+// ==== Task 1 ====
 
 // const items = document.querySelectorAll('.item');
+// console.log(`Number of categories: ${items.length}`);
+
 // Array.prototype.forEach.call(items, (element) => {
 //     const title = element.querySelector('h2').innerHTML;
 //     const itemsLength = element.querySelectorAll('li').length;
-//     console.log(`Категория: ${title} / Количество элементов: ${itemsLength}`);
+//     console.log(`Category: ${title} \n Elements: ${itemsLength}`);
 // });
+
+// ==== Task 2 ====
+
+const ingredients = [
+    "Potatoes",
+    "Mushrooms",
+    "Garlic",
+    "Tomatos",
+    "Herbs",
+    "Condiments",
+  ];
+  
+  const list = document.querySelector("#ingredients");
+  
+  const elements = ingredients.map(ingredient => {
+    const listItem = document.createElement("li")
+    listItem.textContent = ingredient;
+    listItem.classList.add("item");
+
+    return listItem
+  });
+
+  list.append(...elements);
+  
+  console.log(list);
+
+
+//   const list = elements => {
+//     return elements.map(element => {
+//         const listItem = document.createElement("li");
+//         listItem.textContent = element;
+//         listItem.classList.add("item");
+    
+//         return listItem;
+//   });
+// };
+
+// const elements = list(newList);
+// newlist.append(...elements);
+  
+// console.log(list);
+// console.log(elements);
